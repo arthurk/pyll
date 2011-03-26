71ec51768001f7f1895d907eba8d4176ef85d358
Arthur Koziel <arthur@arthurkoziel.com>
2011-03-26 22:34:02 +0100

    cleaning up

--- src/lanyon/app.py
+++ src/lanyon/app.py
@@ -2,7 +2,6 @@ import BaseHTTPServer
 from codecs import open
 from collections import defaultdict, namedtuple
 from datetime import datetime
-from fnmatch import fnmatch
 import imp
 import logging
 from optparse import OptionParser
@@ -15,65 +14,19 @@ import sys
 import time
 
 from lanyon import __version__, parser
+from lanyon.url import get_url
 from lanyon.utils import copy_file, walk_ignore, OrderedDict
 from lanyon.template import Jinja2Template, TemplateException
 from lanyon.server import LanyonHTTPRequestHandler
-from lanyon.url import registry
 
 LOGGING_LEVELS = {'info': logging.INFO, 'debug': logging.DEBUG}
-
 
 class Site(object):
 
     def __init__(self, settings):
         self.settings = settings
-        self.articles = []
-        self.media = []
-
-
-    def _get_output_path(self, url):
-        """
-        Return the absolute output path for an article.
-
-        Keyword arguments:
-        url -- the parsed "url" header from the article
-        """
-        if isabs(url):
-            # omit starting slash char; if we wouldn't do this, the
-            # join() below would return a path that starts from 
-            # the root of the filesystem instead of the output_dir.
-            url = url[1:]
-        if not basename(url):
-            # url is a directory -> write to 'index.html'
-            output_path = join(url, 'index.html')
-        else:
-            # url is a filename
-            output_path = url
-        return join(self.settings['output_dir'], output_path)
-
-
-
-    def _get_url_func(self, path, url):
-        """
-        Return the function that generates the url.
-
-        Keyword arguments:
-        path -- absolute path to the input file
-        url -- url specified in the article's header
-        """
-        path = relpath(path, self.settings['input_dir'])
-        for pattern in reversed(registry):
-            rules = registry[pattern]
-            if fnmatch(path, pattern):
-                if url in rules:
-                    return rules[url]
-                elif pattern == '*' and url != 'default':
-                    # special case: user entered something but it isn't
-                    # a url function -> assume its an output path 
-                    return lambda **x: url
-                elif 'default' in rules:
-                    return rules['default']
-
+        self.pages = []
+        self.static_files = []
 
     def _copy_media(self):
         # media that isn't associated with articles
@@ -88,7 +41,6 @@ class Site(object):
                            dirname(article.output_path),
                            relpath(medium, dirname(article.input_path)))
                 copy_file(medium, dst)
-
 
     def _is_public(self, article):
         """Return True if article is public"""
@@ -178,19 +130,11 @@ class Site(object):
                                     ([], []))[1].extend(static)
         return data
 
-    def _load_custom_urls(self):
-        "Load custom urls from urls.py"
-        path = join(self.settings['lib_dir'], 'urls.py')
-        if exists(path):
-            try:
-                urls = imp.load_source('urls', path)
-            except IOError:
-                logging.debug('couldn\'t load urls from "%s"', path)
-
     def _get_default_headers(self, path):
         """
-        Return the default headers for a page.
+        Returns a dict with the default headers for `path`.
 
+        `path` - the relative path from the project dir to the file
         `title` - titleized version of the filename
         `date` - set to ctime. On unix this is the time of the most recent
                  metadata change; on windows the creation time. If ctime
@@ -201,9 +145,11 @@ class Site(object):
         `url` - set to "default" rule
         `slug` - filename or, if the filename is "index", the dirname
                of the parent directory unless its the top level dir.
+        `output_ext` - the extension of the parsed file
         """
+        output_ext = splitext(path)[1][1:]
         root, filename = split(splitext(path)[0])
-        if filename == 'index' and root != self.settings['input_dir']:
+        if filename == 'index' and root != self.settings['project_dir']:
             slug = basename(root)
         else:
             slug = filename
@@ -213,31 +159,51 @@ class Site(object):
         except OSError:
             # use the current date if the ctime cannot be accessed
             date = datetime.now()
-        return dict(title=title, date=date, status='live', slug=slug,
-                    template='default.html', url='default')
+        return dict(path=relpath(path, self.settings['project_dir']),
+                    title=title, date=date, status='live',
+                    slug=slug, template='default.html', url='default',
+                    output_ext=output_ext)
+
+    def _get_output_path(self, url):
+        """
+        Returns the absolute output path for a page.
+
+        Keyword arguments:
+            url -- the parsed "url" header
+        """
+        if isabs(url):
+            # omit starting slash char; if we wouldn't do this, the
+            # join() below would return a path that starts from 
+            # the root of the filesystem instead of the output_dir.
+            url = url[1:]
+        if not basename(url):
+            # url is a directory -> write to 'index.html'
+            output_path = join(url, 'index.html')
+        else:
+            # url is a filename
+            output_path = url
+        return join(self.settings['output_dir'], output_path)
 
     def _parse(self, input_data):
-        self._load_custom_urls()
-        Page = namedtuple('Page',
-                          'headers, body, input_path, output_path, media')
+        "Parses the input data"
         for input_dir in input_data:
-            pages, static = input_data[input_dir]
-            print pages, static
+            pages, static_files = input_data[input_dir]
 
-            # special case: static files from the root input dir are not
-            # associated with any pages
+            # special case: static files at the top level of the project dir 
+            # are not associated with any pages
             if input_dir == self.settings['project_dir']:
-                self.static = static
+                self.static_files = static_files
                 static = []
 
             for path in pages:
-                # initialize parser cls
-                f = open(path, 'r', encoding='utf8')
-                parser_cls = parser.get_parser_for_filename(path)
-                parser_inst = parser_cls(self.settings, f.read())
-                f.close()
+                page = dict(static_files=static_files,)
+                page.update(self._get_default_headers(path))
 
                 # parse the page
+                parser_cls = parser.get_parser_for_filename(page['path'])
+                with open(path, 'r', encoding='utf8') as f:
+                    parser_inst = parser_cls(self.settings, f.read())
+
                 try:
                     parsed = parser_inst.parse()
                 except parser.ParserException as parser_error:
@@ -245,40 +211,38 @@ class Site(object):
                     logging.error('skipping article "%s"', path)
                     continue
 
-                output_ext = parser_cls.output_ext or splitext(path)[1][1:]
+                # update the values in the page dict
+                page.update(output_ext=parser_cls.output_ext,
+                            body=parsed[1],
+                            **parsed[0])
 
-                # replace default headers with custom headers from page
-                headers = self._get_default_headers(path)
-                headers.update(parsed[0])
-
-                # don't process drafts and future-dated articles
-                if headers['status'] == 'draft' or \
-                   headers['date'] > datetime.today():
+                # skip drafts
+                if page['status'] == 'draft':
+                    logging.debug('skipping %s (draft)', path)
                     continue
+                # skip pages with a date that is in the future
+                elif page['date'] > datetime.today():
+                    logging.debug('skipping %s (future-dated)', path)
+                    continue
+                
+                # resolve url pattern
+                page['url'] = get_url(page)
 
-                # resolve custom url pattern
-                url_func = self._get_url_func(path, headers['url'])
-                headers['url'] = url_func(**dict(
-                    path=relpath(path, self.settings['input_dir']),
-                    extension=output_ext,
-                    headers=headers,
-                ))
-                # variable subst
+                # substitute variables in url pattern
                 substitute_vars = dict(
-                    year=headers['date'].year,
-                    month=headers['date'].strftime('%m'),
-                    day=headers['date'].strftime('%d'),
-                    slug=headers['slug'],
-                    ext=output_ext,
+                    year=page['date'].year,
+                    month=page['date'].strftime('%m'),
+                    day=page['date'].strftime('%d'),
+                    slug=page['slug'],
+                    ext=page['output_ext'],
                 )
-                headers['url'] = Template(headers['url']).safe_substitute(
-                                    substitute_vars)
-                output_path = self._get_output_path(headers['url'])
-                self.articles.append(Article(headers=headers,
-                                             body=parsed[1],
-                                             input_path=path,
-                                             output_path=output_path,
-                                             media=media))
+                tmpl = Template(page['url'])
+                page['url'] = tmpl.safe_substitute(substitute_vars)
+
+                # get the output path for the page
+                #output_path = self._get_output_path(headers['url'])
+
+                self.pages.append(page)
                 sys.stdout.write('.')
         sys.stdout.write('\n')
 
@@ -305,31 +269,19 @@ class Site(object):
         """
 
 def main():
-    # parse options
-    parser = OptionParser(usage="%prog", version="%prog " + __version__)
-    parser.add_option('--date-format',
-                      help='Date format used in articles (default: %Y-%m-%d)',
-                      default='%Y-%m-%d')
-    parser.add_option('-l', '--logging-level', help='Logging level')
-    """
-    parser.add_option('-p', '--port',
-                      help='Webserver port number (default: 8000)',
-                      default=8000, type='int')
-    parser.add_option('-s', '--serve', help='Use local webserver',
-                      action="store_true", dest="serve")
-    parser.add_option('--noreload', help='Do NOT use the autoreloader',
-                      action="store_true", dest="noreload")
-    """
+    parser = OptionParser(version="%prog " + __version__)
+    parser.add_option('-l', '--logging-level',
+                      help="sets the logging level. 'info' (default) or 'debug'")
     (options, args) = parser.parse_args()
 
     project_dir = abspath(getcwd())
     settings = {'project_dir': project_dir,
-                #'input_dir': join(project_dir, 'input'),
                 'output_dir': join(project_dir, '_output'),
                 'template_dir': join(project_dir, '_templates'),
                 'lib_dir': join(project_dir, '_lib'),
+                'url_path': join(project_dir, '_lib', 'urls.py'),
                 'build_time': datetime.today(),
-                'date_format': options.date_format}
+                'date_format': '%Y-%m-%d'}
 
     # configure logging
     logging_level = LOGGING_LEVELS.get(options.logging_level, logging.INFO)
@@ -338,6 +290,14 @@ def main():
                         datefmt='%Y-%m-%d %H:%M:%S')
     logging.debug('default settings %s', settings)
 
+    # import custom urls
+    try:
+        urls = imp.load_source('urls', settings['url_path'])
+    except IOError as e:
+        logging.debug('couldn\'t load urls from "%s": %s',
+                      settings['url_path'], e)
+
+    # initialize site
     site = Site(settings)
     site.run()
 
--- src/lanyon/url.py
+++ src/lanyon/url.py
@@ -1,8 +1,35 @@
-from os.path import splitext, split
-from lanyon.utils import OrderedDict
+from fnmatch import fnmatch
+from os.path import splitext, split, relpath
 
+from lanyon.utils import OrderedDict
 registry = OrderedDict()
+
+def get_url(page):
+    "Returns the final output url string for `page`"
+    urlfunc = get_url_func(page)
+    return urlfunc(page)
+
+def get_url_func(page):
+    """
+    Returns the entry from the url registry that matches the specified
+    url header value.
+    """
+    path = page['path']
+    url = page['url']
+    for pattern in reversed(registry):
+        rules = registry[pattern]
+        if fnmatch(path, pattern):
+            if url in rules:
+                return rules[url]
+            elif pattern == '*' and url != 'default':
+                # special case: user entered something but it isn't
+                # a url function -> assume its an output path 
+                return lambda **x: url
+            elif 'default' in rules:
+                return rules['default']
+
 def register(func=None, match='*'):
+    "A registry for url rules"
     def decorated(func):
         # this returns the final, decorated function,
         # regardless of how it was called
@@ -20,9 +47,10 @@ def register(func=None, match='*'):
     return decorated(func)
 
 @register
-def default(**kwargs):
-    path = kwargs['path']
-    ext = kwargs['extension']
+def default(page):
+    "default url rule"
+    path = page['path']
+    ext = page['output_ext']
     url = splitext(path)[0] + '.' + ext
     head, tail = split(url)
     if tail == 'index.html':
@@ -31,5 +59,5 @@ def default(**kwargs):
     return url
 
 @register
-def pretty(**kwargs):
+def pretty(page):
     return '$year/$month/$day/$slug/'
